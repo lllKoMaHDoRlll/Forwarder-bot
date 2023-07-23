2bef2a281955337bec0c85abb4b5215c890ec635
Koma <aleksandr.orlichenya@gmail.com>
2023-07-23 21:10:10 +0300

    switch to lastest version of APScheduler

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -1,23 +1,23 @@
 from typing import NoReturn, Callable
 
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.sync import Scheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
 from backend.data_classes import ConfigData
 
 
-class Scheduler:
+class Scheduler_:
     def __init__(self, config_data: ConfigData):
         self.config_data = config_data
-        self.scheduler = BackgroundScheduler()
+        self.scheduler = Scheduler()
 
     def _parse_trigger(self) -> IntervalTrigger | NoReturn:
         trigger_data = self.config_data.trigger
         type_ = trigger_data["type"]
-        value = int(trigger_data["value"])
+        value = trigger_data["value"]
         match type_:
             case "interval":
-                trigger = IntervalTrigger(seconds=value)
+                trigger = IntervalTrigger(seconds=int(value))
             case _:
                 raise ValueError("Unsupported trigger type.")
 
@@ -25,7 +25,7 @@ class Scheduler:
 
     def add_schedule(self, task: Callable, *args):
         trigger = self._parse_trigger()
-        self.scheduler.add_job(func=task, trigger=trigger, args=args)
+        self.scheduler.add_schedule(func_or_task_id=task, trigger=trigger, args=args)
 
     def run_scheduler(self):
-        self.scheduler.start()
+        self.scheduler.run_until_stopped()
