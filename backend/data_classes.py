e95582b51e3b9693bdcd493890411bc060e892e4
Koma <aleksandr.orlichenya@gmail.com>
2023-07-23 17:26:29 +0300

    create PlainTextPost and PhotoPost dataclasses

--- backend/data_classes.py
+++ backend/data_classes.py
@@ -9,3 +9,13 @@ class ConfigData:
     to_group_id: int
     vk_api_version: str
 
+
+@dataclass
+class PlainTextPost:
+    text: str
+
+@dataclass
+class PhotoPost:
+    photo_url: str
+    caption: str | None = None
+
